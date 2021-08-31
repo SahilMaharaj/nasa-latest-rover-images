d0d8193728c52b4121316d1c4345f8b28781c457
unknown <maharajsahil9@gmail.com>
2021-08-31 16:08:45 +0200

    Removed modal from mainjs

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import VModal from 'vue-js-modal/dist/ssr.nocss'
+
 
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
-  Vue.use(VModal)
+
 
   // Add Google font
   head.link.push ({

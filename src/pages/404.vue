75a27dc8eeb7db3ddff258065fcb011965522633
unknown <maharajsahil9@gmail.com>
2021-08-31 15:50:14 +0200

    Added in 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,15 @@
+<template>
+    <Layout>
+        <h1>Oops the page you're looking for does not exist...</h1>
+    </Layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
